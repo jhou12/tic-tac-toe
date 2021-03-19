3229ac956636ccffb761fc7dc5e4aa61ea236fee
Jenny Hou <68251937+jhou12@users.noreply.github.com>
2021-03-18 23:20:10 -0400

    Initial commit

---
+++ client/src/Styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+height: 100vh;
+font-family: 'Roboto', sans-serif;
+font-size: 20px;
+color: mediumpurple;
+/* background-color: black; */
+`;
+
+export const Column = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 10%;
+`;
+
+export const Title = styled.div`
+font-size: 35px;
+font-weight: 400;
+padding: 5px;
+`;
+
+export const Emoji = styled.div`
+font-size: 35px;
+padding: 5px;
+`;
